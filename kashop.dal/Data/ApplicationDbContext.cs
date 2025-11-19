e000da920986f56f68dca58105c452abd3d2d6b8
Jannat-Dawabsheh <S11923966@stu.najah.edu>
2025-11-19 16:25:27 +0200

    first commit

---
+++ kashop.dal/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using kashop.dal.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kashop.dal.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Category>Categories { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+        {
+        }
+    }
+}
